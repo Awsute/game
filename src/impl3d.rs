1ea20703c68ef5bdbca660ff203215272acf4d97
Awsute <63113841+Awsute@users.noreply.github.com>
2021-04-15 14:15:27 -0400

    optimizations

--- src/impl3d.rs
+++ src/impl3d.rs
@@ -1,3 +1,4 @@
+use std::ops;
 pub trait Vec3{
     fn scale(&self, scalar : Self)->Self;
     fn add(&self, a : Self)->Self;
@@ -7,64 +8,64 @@ pub trait Vec3{
     fn negative(&self)->Self;
     fn cross_product(&self, c : Self)->Self;
     fn dot_product(&self, d : Self)->f32;
-    fn multiply_mat(&self, mat : [[f32;3];3])->Self;
+    fn multiply_mat(&self, mat : [[f32;4];4])->Self;
 }
-impl Vec3 for [f32;3]{
-    fn scale(&self, scalar : [f32;3])->[f32;3]{
-        return [self[0]*scalar[0], self[1]*scalar[1], self[2]*scalar[2]];
+impl Vec3 for [f32;4]{
+    fn scale(&self, scalar : [f32;4])->[f32;4]{
+        return [self[0]*scalar[0], self[1]*scalar[1], self[2]*scalar[2], self[3]];
     }
-    fn add(&self, a : [f32;3])->[f32;3]{
-        return [self[0]+a[0], self[1]+a[1], self[2]+a[2]];
+    fn add(&self, a : [f32;4])->[f32;4]{
+        return [self[0]+a[0], self[1]+a[1], self[2]+a[2], self[3]];
     }
-    fn subtract(&self, a : [f32;3])->[f32;3]{
-        return [self[0]-a[0], self[1]-a[1], self[2]-a[2]];
+    fn subtract(&self, a : [f32;4])->[f32;4]{
+        return [self[0]-a[0], self[1]-a[1], self[2]-a[2], self[3]];
     }
     fn magnitude(&self)->f32{
         return (self[0].powi(2) + self[1].powi(2) + self[2].powi(2)).sqrt();
     }
-    fn normalize(&self)->[f32;3]{
+    fn normalize(&self)->[f32;4]{
         let m = self.magnitude();
-        return [self[0]/m, self[1]/m, self[2]/m];
+        return [self[0]/m, self[1]/m, self[2]/m, self[3]];
     }
-    fn negative(&self)->[f32;3]{
-        return [-self[0], -self[1], -self[2]];
+    fn negative(&self)->[f32;4]{
+        return [-self[0], -self[1], -self[2], 1.0];
     }
-    fn cross_product(&self, c: [f32;3])->[f32;3]{
-        return [-self[1]*c[2] + c[1]*self[2], -self[2]*c[0] + c[2]*self[0], -self[0]*c[1] + c[0]*self[1]];
+    fn cross_product(&self, c: [f32;4])->[f32;4]{
+        return [-self[1]*c[2] + c[1]*self[2], -self[2]*c[0] + c[2]*self[0], -self[0]*c[1] + c[0]*self[1], 1.0];
     }
     fn dot_product(&self, d: Self)->f32{
         return self[0]*d[0] + self[1]*d[1] + self[2]*d[2];
     }
-    fn multiply_mat(&self, m : [[f32;3];3])->[f32;3]{
+    fn multiply_mat(&self, m : [[f32;4];4])->[f32;4]{
         return [
-            self[0] * m[0][0] + self[1] * m[1][0] + self[2] * m[2][0],
-            self[0] * m[0][1] + self[1] * m[1][1] + self[2] * m[2][1],
-            self[0] * m[0][2] + self[1] * m[1][2] + self[2] * m[2][2] 
+            self[0] * m[0][0] + self[1] * m[1][0] + self[2] * m[2][0] + self[3] * m[3][0],
+            self[0] * m[0][1] + self[1] * m[1][1] + self[2] * m[2][1] + self[3] * m[3][1],
+            self[0] * m[0][2] + self[1] * m[1][2] + self[2] * m[2][2] + self[3] * m[3][2],
+            self[0] * m[0][3] + self[1] * m[1][3] + self[2] * m[2][3] + self[3] * m[3][3]
         ];
     }
 }
-
 pub trait Tri3d{
-    fn normal(&self)->[f32;3];
-    fn translate(&self, t:[f32;3])->[[f32;3];3];
-    fn scale(&self, t:[f32;3])->[[f32;3];3];
-    fn center(&self)->[f32;3];
-    fn multiply_mat(&self, m:[[f32;3];3])->Self;
+    fn normal(&self)->[f32;4];
+    fn translate(&self, t:[f32;4])->Self;
+    fn scale(&self, t:[f32;4])->Self;
+    fn center(&self)->[f32;4];
+    fn multiply_mat(&self, m:[[f32;4];4])->Self;
 }
-impl Tri3d for [[f32;3];3]{
-    fn normal(&self)->[f32;3]{
+impl Tri3d for [[f32;4];3]{
+    fn normal(&self)->[f32;4]{
         return self[2].subtract(self[0]).cross_product(self[1].subtract(self[0])).normalize();//sheeeesh
     }
-    fn translate(&self, t : [f32;3])->[[f32;3];3]{
+    fn translate(&self, t : [f32;4])->Self{
         return [self[0].add(t), self[1].add(t), self[2].add(t)];
     }
-    fn scale(&self, t : [f32;3])->[[f32;3];3]{
+    fn scale(&self, t : [f32;4])->Self{
         return [self[0].scale(t), self[1].scale(t), self[2].scale(t)];
     }
-    fn center(&self)->[f32;3]{
-        return self[0].add(self[1].add(self[2])).scale([1.0/3.0, 1.0/3.0, 1.0/3.0])
+    fn center(&self)->[f32;4]{
+        return self[0].add(self[1]).add(self[2]).scale([1.0/3.0, 1.0/3.0, 1.0/3.0, 1.0])
     }
-    fn multiply_mat(&self, m:[[f32;3];3])->Self{
+    fn multiply_mat(&self, m:[[f32;4];4])->Self{
         return [
             self[0].multiply_mat(m),
             self[1].multiply_mat(m),
--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,57 @@ use sdl2::gfx::primitives::DrawRenderer;
 mod impl3d;
 use impl3d::Vec3;
 use impl3d::Tri3d;
-trait operations3x3{
+
+fn gen_terrain(start : [f32;4], end : [f32;4], increment : f32, func : &dyn Fn(f32, f32)->f32)->Vec<[f32;4]>{
+    let mut r : Vec<[f32;4]> = Vec::new();
+    for i in start[0] as i32..end[0] as i32{
+        for j in start[2] as i32..end[2] as i32{
+            if i%increment as i32 == 0 && j%increment as i32 == 0{
+                let x = start[0]+i as f32;
+                let z = start[2]+j as f32;
+                let y = func(x, z) + start[1];
+                r.push([x, y, z, 1.0]);
+            }
+
+        }
+    }
+    return r;
+}
+
+
+trait operations4x4{
     fn multiply(self, mat : Self)->Self;
 }
-impl operations3x3 for [[f32;3];3]{
-    fn multiply(self, mat : [[f32;3];3])->[[f32;3];3]{
+impl operations4x4 for [[f32;4];4]{
+    fn multiply(self, mat : Self)->Self{
         return [
-            [self[0][0]*mat[0][0] + self[0][1]*mat[1][0] + self[0][2]*mat[2][0], self[0][0]*mat[0][1] + self[0][1]*mat[1][1] + self[0][2]*mat[2][1], self[0][0]*mat[0][2] + self[0][1]*mat[1][2] + self[0][2]*mat[2][2]],
-            [self[1][0]*mat[0][0] + self[1][1]*mat[1][0] + self[1][2]*mat[2][0], self[1][0]*mat[0][1] + self[1][1]*mat[1][1] + self[1][2]*mat[2][1], self[1][0]*mat[0][2] + self[1][1]*mat[1][2] + self[1][2]*mat[2][2]],
-            [self[2][0]*mat[0][0] + self[2][1]*mat[1][0] + self[2][2]*mat[2][0], self[2][0]*mat[0][1] + self[2][1]*mat[1][1] + self[2][2]*mat[2][1], self[2][0]*mat[0][2] + self[2][1]*mat[1][2] + self[2][2]*mat[2][2]],
+            [
+                self[0][0]*mat[0][0] + self[0][1]*mat[1][0] + self[0][2]*mat[2][0] + self[0][3]*mat[3][0], 
+                self[0][0]*mat[0][1] + self[0][1]*mat[1][1] + self[0][2]*mat[2][1] + self[0][3]*mat[3][1], 
+                self[0][0]*mat[0][2] + self[0][1]*mat[1][2] + self[0][2]*mat[2][2] + self[0][3]*mat[3][2],
+                self[0][0]*mat[0][3] + self[0][1]*mat[1][3] + self[0][2]*mat[2][3] + self[0][3]*mat[3][3],
+            ],
+
+            [
+                self[1][0]*mat[0][0] + self[1][1]*mat[1][0] + self[1][2]*mat[2][0] + self[1][3]*mat[3][0], 
+                self[1][0]*mat[0][1] + self[1][1]*mat[1][1] + self[1][2]*mat[2][1] + self[1][3]*mat[3][1], 
+                self[1][0]*mat[0][2] + self[1][1]*mat[1][2] + self[1][2]*mat[2][2] + self[1][3]*mat[3][2],
+                self[1][0]*mat[0][3] + self[1][1]*mat[1][3] + self[1][2]*mat[2][3] + self[1][3]*mat[3][3],
+            ],
+
+            [
+                self[2][0]*mat[0][0] + self[2][1]*mat[1][0] + self[2][2]*mat[2][0] + self[2][3]*mat[3][0], 
+                self[2][0]*mat[0][1] + self[2][1]*mat[1][1] + self[2][2]*mat[2][1] + self[2][3]*mat[3][1], 
+                self[2][0]*mat[0][2] + self[2][1]*mat[1][2] + self[2][2]*mat[2][2] + self[2][3]*mat[3][2],
+                self[2][0]*mat[0][3] + self[2][1]*mat[1][3] + self[2][2]*mat[2][3] + self[2][3]*mat[3][3],
+            ],
+
+            [
+                self[3][0]*mat[0][0] + self[3][1]*mat[1][0] + self[3][2]*mat[2][0] + self[3][3]*mat[3][0], 
+                self[3][0]*mat[0][1] + self[3][1]*mat[1][1] + self[3][2]*mat[2][1] + self[3][3]*mat[3][1], 
+                self[3][0]*mat[0][2] + self[3][1]*mat[1][2] + self[3][2]*mat[2][2] + self[3][3]*mat[3][2],
+                self[3][0]*mat[0][3] + self[3][1]*mat[1][3] + self[3][2]*mat[2][3] + self[3][3]*mat[3][3],
+            ],
         ]
     }
 }
@@ -38,10 +80,10 @@ impl operations3x3 for [[f32;3];3]{
 #[derive(Copy, Clone)]
 struct Camera{
     fov : f32,
-    pos : [f32;3],
-    rot : [f32;3],
-    vel : [f32;3],
-    rot_vel : [f32;3],
+    pos : [f32;4],
+    rot : [f32;4],
+    vel : [f32;4],
+    rot_vel : [f32;4],
     vll : f32
 }
 struct Engine{
@@ -54,34 +96,38 @@ struct Engine{
 }
 
 impl Engine{
-    fn matrix(&self)->[[f32;3];3]{
-        let t = (self.camera.fov*180.0/(2.0*std::f32::consts::PI)).tan();
-        let zr = self.render_distance/(self.render_distance-self.clip_distance);
+    fn matrix3d(&self)->[[f32;4];4]{
+        let t = ((self.camera.fov/2.0)*(std::f32::consts::PI/180.0)).tan();
+        let zratio = self.render_distance/(self.render_distance-self.clip_distance);
         return [
-            [(self.window_height/self.window_width)/t, 0.0, 0.0],
-            [0.0, 1.0/t, 0.0],
-            [0.0, 0.0, -self.clip_distance*zr]
+            [-1.0/(t*self.window_width/self.window_height), 0.0, 0.0, 0.0],
+            [0.0, -1.0/t, 0.0, 0.0],
+            [0.0, 0.0, zratio, 1.0],
+            [0.0, 0.0, -self.clip_distance*zratio, 0.0]
         ];
     }
-    fn x_rot(angle : f32)->[[f32;3];3]{
+    fn x_rot(angle : f32)->[[f32;4];4]{
         return [
-            [1.0, 0.0, 0.0],
-            [0.0, angle.cos(), angle.sin()],
-            [0.0, -angle.sin(), angle.cos()]
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, angle.cos(), angle.sin(), 0.0],
+            [0.0, -angle.sin(), angle.cos(), 0.0],
+            [0.0, 0.0, 0.0, 1.0]
         ];
     }
-    fn y_rot(angle : f32)->[[f32;3];3]{
+    fn y_rot(angle : f32)->[[f32;4];4]{
         return [
-            [angle.cos(), 0.0, -angle.sin()],
-            [0.0, 1.0, 0.0],
-            [angle.sin(), 0.0, angle.cos()]
+            [angle.cos(), 0.0, -angle.sin(), 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [angle.sin(), 0.0, angle.cos(), 0.0],
+            [0.0, 0.0, 0.0, 1.0]
         ];
     }
-    fn z_rot(angle : f32)->[[f32;3];3]{
+    fn z_rot(angle : f32)->[[f32;4];4]{
         return [
-            [angle.cos(), -angle.sin(), 0.0],
-            [angle.sin(), angle.cos(), 0.0],
-            [0.0, 0.0, 1.0]
+            [angle.cos(), -angle.sin(), 0.0, 0.0],
+            [angle.sin(), angle.cos(), 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]
         ];
     }
     /*[
@@ -89,11 +135,12 @@ impl Engine{
         [z.sin()*y.cos(), z.cos(), z.sin()*-y.sin()],
         [0.0, 0.0, y.cos()]
     ]*/
-    fn rot_zyx(x : f32, y : f32, z : f32)->[[f32;3];3]{
+    fn rot_zyx(x : f32, y : f32, z : f32)->[[f32;4];4]{
         return Engine::z_rot(z).multiply(Engine::y_rot(y)).multiply(Engine::x_rot(x));
     }
 
 }
+
 
 
 trait draw_tri{
@@ -110,47 +157,32 @@ impl draw_tri for WindowCanvas{
 }
 #[derive(Clone)]
 struct Object{
-    tris : Vec<[[f32;3];3]>,
-    rot : [f32;3],
-    vel : [f32;3],
-    rot_vel : [f32;3]
+    tris : Vec<[[f32;4];3]>,
+    rot : [f32;4],
+    vel : [f32;4],
+    rot_vel : [f32;4]
 }
 
 impl Object{
-    fn new(mut tris:Vec<[[f32;3];3]>, rot:[f32;3])->Self{
-        return Object{tris, rot, vel : [0.0, 0.0, 0.0], rot_vel : [0.0, 0.0, 0.0]};
+    fn new(mut tris:Vec<[[f32;4];3]>, rot:[f32;4])->Self{
+        return Object{tris, rot, vel : [0.0, 0.0, 0.0, 0.0], rot_vel : [0.0, 0.0, 0.0, 0.0]};
     }
-    fn center(&self)->[f32;3]{
-        let mut c = [0.0, 0.0, 0.0];
+    fn center(&self)->[f32;4]{
+        let mut c = [0.0, 0.0, 0.0, 1.0];
         let n = self.tris.len() as f32;
         for tri in &self.tris{
             c = c.add(tri.center());
         }
-        return c.scale([1.0/n, 1.0/n, 1.0/n])
+        return c.scale([1.0/n, 1.0/n, 1.0/n, 1.0])
     }
-    fn sort_tris(&self)->Vec<[[f32;3];3]>{
-        let mut ts = self.tris.clone();
-        for i in 0..ts.len(){
-            
-            for o in 0..ts.len(){
-                if i != o{
-                    let t1 = ts[i];
-                    let t2 = ts[o];
-                    //sort
-                    if t1.center().magnitude() > t2.center().magnitude(){
-                        ts[o] = t1;
-                        ts[i] = t2;
-                    }
-                }
-            }
-        }
-        return ts;
+    fn sort_tris(&mut self){
+        &self.tris.sort_by(|a,b| b.center().magnitude().partial_cmp(&a.center().magnitude()).unwrap());
     }
     fn load_obj_file(file_path:String)->Self{
         let file = File::open(file_path).unwrap();
         let reader = BufReader::new(file);
-        let mut ts : Vec<[[f32;3];3]> = Vec::new();
-        let mut points : Vec<[f32;3]> = Vec::new();
+        let mut ts : Vec<[[f32;4];3]> = Vec::new();
+        let mut points : Vec<[f32;4]> = Vec::new();
         
         for line in reader.lines() {
             let ln = Box::leak(line.unwrap().into_boxed_str());
@@ -162,6 +194,7 @@ impl Object{
                             vals[1].parse::<f32>().unwrap(),
                             vals[2].parse::<f32>().unwrap(),
                             vals[3].parse::<f32>().unwrap(),
+                            1.0
                         ]
                     );
                 } else if vals[0].to_string() == "f".to_string() {
@@ -175,38 +208,26 @@ impl Object{
                 }
             }
         }
-        return Object{tris:ts, rot:[0.0, 0.0, 0.0], vel:[0.0, 0.0, 0.0], rot_vel:[0.0, 0.0, 0.0]};
+        return Object{tris:ts, rot:[0.0, 0.0, 0.0, 0.0], vel:[0.0, 0.0, 0.0, 0.0], rot_vel:[0.0, 0.0, 0.0, 0.0]};
     }
-    fn translate(&self, t : [f32;3])->Self{
-        let mut s = self.tris.clone();
-        for i in 0..s.len(){
-            s[i] = s[i].translate(t);
+    fn translate(&self, t : [f32;4])->Self{
+        let mut s = Vec::new();
+        for i in &self.tris{
+            s.push(i.translate(t));
         }
         return Object{tris:s, rot:self.rot, vel:self.vel, rot_vel:self.rot_vel};
     }
-    fn translatem(mut self, t : [f32;3])->Self{
-        for i in 0..self.tris.len(){
-            self.tris[i] = self.tris[i].translate(t);
-        }
-        return self;
-    }
-    fn scale(&self, t : [f32;3])->Self{
-        let mut s = self.tris.clone();
-        for i in 0..s.len(){
-            s[i] = s[i].scale(t);
+    fn scale(&self, t : [f32;4])->Self{
+        let mut s = Vec::new();
+        for i in &self.tris{
+            s.push(i.scale(t));
         }
         return Object{tris:s, rot:self.rot, vel:self.vel, rot_vel:self.rot_vel};
     }
-    fn scalem(mut self, t : [f32;3])->Self{
+    fn rotate_point(&self, deg : [f32;4], point : [f32;4])->Self{
+        let mut ts = Vec::new();
         for i in 0..self.tris.len(){
-            self.tris[i] = self.tris[i].scale(t);
-        }
-        return self;
-    }
-    fn rotate_point(&self, deg : [f32;3], point : [f32;3])->Self{
-        let mut ts = self.tris.clone();
-        for i in 0..ts.len(){
-            ts[i] = ts[i].translate(point.negative());
+            ts.push(self.tris[i].translate(point.negative()));
             if deg[2] != 0.0{
                 ts[i] = ts[i].multiply_mat(Engine::z_rot(deg[2]));
             }
@@ -220,12 +241,6 @@ impl Object{
         }
         return Object{tris:ts, rot:self.rot.add(deg), vel:self.vel, rot_vel:self.rot_vel};
     }
-    fn rotate_origin(&self, deg : [f32;3])->Self{
-        return self.rotate_point(deg, [0.0, 0.0, 0.0]);
-    }
-    fn rotate_self(&self, deg : [f32;3])->Self{
-        return self.rotate_point(deg, self.center());
-    }
 }
 
 
@@ -237,8 +252,8 @@ fn main() {
     let audio_subsystem = sdl_context.audio().unwrap();
     let sdl_image_context = image::init(image::InitFlag::all());
 
-    let mut window = video_subsystem.window("game", 0, 0)
-        .fullscreen_desktop()
+    let mut window = video_subsystem.window("game", 500, 500)
+        //.fullscreen_desktop()
         .build()
         .map_err(|e| e.to_string())
         .unwrap();
@@ -254,23 +269,28 @@ fn main() {
     let screen_height = canvas.output_size().unwrap().1 as i32;
     let mut event_pump = sdl_context.event_pump().unwrap();
     let texture_creator = canvas.texture_creator();
-    let mut obj = Object::load_obj_file("assets/character.obj".to_string()).scale([25.0, 25.0, 25.0]).translate([0.0, 0.0, 15.0]);
-    obj.tris = obj.sort_tris();
     let mut objs : Vec<Object> = Vec::new();
-    objs.push(obj);
     
+
+    
+    objs.push(Object::load_obj_file("assets/character.obj".to_string()).scale([10.0, 10.0, 10.0, 1.0]).translate([0.0, 0.0, 15.0, 0.0]));
+    objs[0].rot_vel = [0.0, 0.0, 60_f32.to_radians(), 0.0];
+    objs.push(Object::load_obj_file("assets/character.obj".to_string()).scale([10.0, 10.0, 10.0, 1.0]).translate([20.0, 0.0, 15.0, 0.0]));
+    objs[1].rot_vel = [0.0, 0.0, 60_f32.to_radians(), 0.0];
+    objs.push(Object::load_obj_file("assets/character.obj".to_string()).scale([10.0, 10.0, 10.0, 1.0]).translate([-20.0, 0.0, 15.0, 0.0]));
+    objs[2].rot_vel = [0.0, 0.0, 60_f32.to_radians(), 0.0];
     let mut player_cam = Camera{
         fov : 75.0,
-        pos : [0.0, 0.0, 0.0],
-        rot : [0.0, 0.0, 0.0],
-        vel : [0.0, 0.0, 0.0],
-        rot_vel : [0.0, 0.0, 0.0],
+        pos : [0.0, 0.0, 0.0, 1.0],
+        rot : [0.0, 0.0, 0.0, 0.0],
+        vel : [0.0, 0.0, 0.0, 0.0],
+        rot_vel : [0.0, 0.0, 0.0, 0.0],
         vll: 90_f32.to_radians()
     };
     let mut engine = Engine{
         camera : player_cam,
-        clip_distance : 0.1,
-        render_distance : 1000.0,
+        clip_distance : 1.0,
+        render_distance : 100.0,
         window_height : canvas.output_size().unwrap().1 as f32,
         window_width : canvas.output_size().unwrap().0 as f32,
         objects : objs
@@ -278,11 +298,10 @@ fn main() {
     };
     let cspeed = 10.0;
     let rspeed = 60.0_f32.to_radians();
-    let mat3d = engine.matrix();
+    let mat3d = engine.matrix3d();
     'running: loop {
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
-        
         
         for event in event_pump.poll_iter() {
             match event {
@@ -376,59 +395,52 @@ fn main() {
         // ok
 
         let cam = engine.camera;
-        engine.camera.pos = cam.pos.add(cam.vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]));
-        engine.camera.rot = cam.rot.add(cam.rot_vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]));
-        
-        for i in 0..engine.objects.len(){
-            //Move from camera
-            if cam.rot_vel[0] != 0.0 || cam.rot_vel[1] != 0.0 || cam.rot_vel[2] != 0.0{
-                engine.objects[i] = engine.objects[i].rotate_origin(cam.rot_vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]).negative());
-            }
-            if cam.vel[0] != 0.0 || cam.vel[1] != 0.0 || cam.vel[2] != 0.0{
-                engine.objects[i] = engine.objects[i].translate(cam.vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]).negative());
-            }
+        engine.camera.rot = cam.rot.add(cam.rot_vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS, 1.0]));
 
-            //Now from the object
-            if engine.objects[i].rot_vel[0] != 0.0 || engine.objects[i].rot_vel[1] != 0.0 || engine.objects[i].rot_vel[2] != 0.0{
-                engine.objects[i] = engine.objects[i].rotate_self(engine.objects[i].rot_vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]));
-            }
-            if engine.objects[i].vel[0] != 0.0 || engine.objects[i].vel[1] != 0.0 || engine.objects[i].vel[2] != 0.0{
-                engine.objects[i] = engine.objects[i].translate(engine.objects[i].vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS]));
+        engine.camera.pos = cam.pos.add(
+            cam.vel.multiply_mat([
+                [engine.camera.rot[1].cos(), engine.camera.rot[2].sin(), -engine.camera.rot[1].sin(), 0.0],
+                [engine.camera.rot[2].sin(), engine.camera.rot[0].cos(), engine.camera.rot[0].sin(), 0.0],
+                [engine.camera.rot[1].sin(), -engine.camera.rot[0].sin(), engine.camera.rot[1].cos(), 0.0],
+                [0.0, 0.0, 0.0, 1.0]
+            ]).scale([1.0/FPS, 1.0/FPS, 1.0/FPS, 1.0])
+        );
+
+        for i in 0..engine.objects.len(){
+            
+            let center = engine.objects[i].center();
+            engine.objects[i] = engine.objects[i].translate(engine.objects[i].vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS, 1.0]));
+            if engine.objects[i].rot_vel.magnitude() != 0.0{
+                engine.objects[i] = engine.objects[i].rotate_point(engine.objects[i].rot_vel.scale([1.0/FPS, 1.0/FPS, 1.0/FPS, 1.0]), center);
             }
             
-            
+
+            let mut obj = engine.objects[i].rotate_point(cam.rot.negative(), cam.pos).translate(cam.pos.negative());
+            obj.sort_tris();
             let mc = |i : f32| -> bool {i > engine.clip_distance || i < engine.render_distance};
-            let light = [0.0, 0.0, -1.0].normalize();
-            for tri in &engine.objects[i].tris{
+            
+            let light = [0.0, 0.0, -1.0, 1.0].normalize();
+            for tri in obj.tris{
                 let normal = tri.normal();
-                
-                if normal.dot_product(tri[0]) < 0.0{
-                    let mut j : [[f32;3];3] = [[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]];
-                    let tri = tri.scale([engine.window_width/2.0, engine.window_height/2.0, 1.0]);
-                    for i in 0..3{
-                        let t = tri[i].multiply_mat(mat3d);
-                        
-                        j[i][0] = t[0];
-                        j[i][1] = t[1];
-                        j[i][2] = tri[i][2];
+                let c = tri.center()[2];
+                if normal.dot_product(tri[0]) <= 0.0 && c > engine.clip_distance && c < engine.render_distance{
+                    let dp = normal.dot_product(light);
+                    if dp >= 0.0{
+                        let t = tri.scale([engine.window_width/2.0, engine.window_height/2.0, 1.0, 1.0]).multiply_mat(mat3d);
+                        let ew = engine.window_width/2.0; let eh = engine.window_height/2.0;
+                        let t03 = t[0][3]; let t13 = t[1][3]; let t23 = t[2][3]; let c = (255.0*dp) as u8;
+                        canvas.draw_triangle(
+                            [(t[0][0]/t03+ew) as i16, (t[0][1]/t03+eh) as i16],     
+                            [(t[1][0]/t13+ew) as i16, (t[1][1]/t13+eh) as i16],
+                            [(t[2][0]/t23+ew) as i16, (t[2][1]/t23+eh) as i16],
+                            Color::from((c, c, c))
+                        );
                     }
-                    if mc(j[0][2])&&mc(j[1][2])&&mc(j[2][2]){
-                        let dp = normal.dot_product(light);
-                        if dp >= 0.0{
-                            
-                            canvas.draw_triangle(
-                                [(j[0][0]/j[0][2]+engine.window_width/2.0) as i16, (j[0][1]/j[0][2]+engine.window_height/2.0) as i16],     
-                                [(j[1][0]/j[1][2]+engine.window_width/2.0) as i16, (j[1][1]/j[1][2]+engine.window_height/2.0) as i16],
-                                [(j[2][0]/j[2][2]+engine.window_width/2.0) as i16, (j[2][1]/j[2][2]+engine.window_height/2.0) as i16],
-                                Color::from(((255.0*dp) as u8, (255.0*dp) as u8, (255.0*dp) as u8))
-                            );
-                        }
-                    }
-                    
                 }
             }   
         }
-        canvas.present();
+        
         std::thread::sleep(std::time::Duration::from_millis((1000.0/FPS) as u64));
+        canvas.present();
     }
 }
